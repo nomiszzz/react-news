32a3920c1eccc130e17c6931fc8acc769db7a61f
zhangfang <simon_zhang@modernmedia.com.cn>
2017-05-22 22:19:54 +0800

    react-news

---
+++ src/js/components/mobile_footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class MobileFooter extends React.Component{
+    render(){
+        return(
+            <div id="mobileFooter">
+                <footer>© 2016 ReactNews. All Rights Reserved.</footer>
+            </div>
+        )
+    }
+}
---
+++ src/js/components/pc_footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Row,Col} from "antd";
+export default class PCFooter extends React.Component{
+    render(){
+        return(
+            <footer>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col span={20}>© 2016 ReactNews. All Rights Reserved.</Col>
+                    <Col span={2}></Col>
+                </Row>
+            </footer>
+        )
+    }
+}
